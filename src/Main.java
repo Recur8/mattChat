b84fcd6833fa5f3358743c8dbafe91430800b9bd
Leonard Reidy <leonard.reidy@postsecondaryanalytics.com>
2015-06-18 00:17:44 +0100

    Add filename to placeholder text

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-/* 
+/* Main.java 
  * This file is a placeholder. I didn't want to expose your code to the public without your permission. 
  * github won't allow us to create a private repository without paying for the service so, either we 
  * allow the repository to be public or we find another service. I like github and I see no reason not
